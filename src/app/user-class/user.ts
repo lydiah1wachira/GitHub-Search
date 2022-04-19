d0f27ee9bdd0c013ca5eb11c0a8ac26bc17c9830
lydiah1wachira <lydiahcedric@gmail.com>
2022-04-19 22:19:00 +0300

    make a blueprint constructor for the user class.

--- src/app/user-class/user.ts
+++ src/app/user-class/user.ts
@@ -1,2 +1,14 @@
 export class User {
+  constructor (
+    public bio:string, 
+    public login:string,
+    public public_repos:number,
+    public created_at:Date,
+    public updated_at:Date,
+    public avatar_url:any,
+    public followers_url:number,
+    public following_url:number, 
+    public html_url:string 
+    ) {}
+  
 }
