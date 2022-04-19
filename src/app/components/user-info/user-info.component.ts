59b0c1631e1d6abfa7f067262325a06eb03e6f20
lydiah1wachira <lydiahcedric@gmail.com>
2022-04-20 01:10:01 +0300

    create a card where the user profile and info will be displayed.

--- src/app/components/user-info/user-info.component.ts
+++ src/app/components/user-info/user-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GitsearchService } from 'src/app/search-service/gitsearch.service';
 
 @Component({
   selector: 'app-user-info',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserInfoComponent implements OnInit {
 
-  constructor() { }
+  username = 'lydiah1wachira'
+  
+  user!:any;
+  repo!:any;
+
+  constructor(private gitsearchService:GitsearchService  ) { }
+
+  getData(newUsername:any):void{
+    this.username = newUsername;
+    this.getRepoData=(newUsername)
+    this.getUserData =(newUsername)
+    this.gitsearchService.getUserRepo(this.username).subscribe((repo)=>{
+      this.repo = repo
+      console.log(repo)
+    })
+    this.gitsearchService.searchUsers(this.username).then((user) =>(this.user = user))
+    console.log(this.username)
+  } 
+
+  getRepoData(username:string):void{
+    this.gitsearchService.getUserRepo(username).subscribe((repo)=>{
+      this.repo = repo
+      console.log(repo)
+    })
+  }
+
+  getUserData(username:string):void{
+    this.gitsearchService.searchUsers(username).then((user) =>(this.user = user))
+    console.log(username)
+  }
 
   ngOnInit(): void {
+
+    this.getRepoData(this.username)
+
+    this.getUserData(this.username)
   }
 
 }
