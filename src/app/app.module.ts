ce40d34cc8f93dd63190ed80931a24857e8a4669
lydiah1wachira <lydiahcedric@gmail.com>
2022-04-19 22:46:28 +0300

    installed bootstrap and imported it to the styles.css file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitSearchFormComponent } from './components/git-search-form/git-search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitSearchFormComponent
   ],
   imports: [
     BrowserModule,
